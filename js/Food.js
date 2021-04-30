95d84fdb8f17d5eca00b30fa65613afbd42c5bb3
Avidan123 <75628503+Avidan123@users.noreply.github.com>
2021-04-30 15:15:42 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,29 @@
+class Food{
+constructor(x,y){
+    var foodStock,lastFed
+
+
+
+this.image=loadImage("images/Milk.png")
+}
+display(){
+var x=80 
+var y =100
+
+imageMode(CENTER)
+image(this.image,)
+
+
+
+}
+getFoodStock(){
+
+
+
+}
+updateFoodStock(){
+
+    
+}
+
+}
